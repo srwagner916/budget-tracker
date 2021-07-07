8771a5bc02fb053105fcf7aa0d2fdc52174ad881
Shay Wagner <srwagner916@gmail.com>
2021-07-07 18:01:37 -0500

    intercepting fetch requests

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,16 @@ const FILES_TO_CACHE = [
   './index.html',
   './css/styles.css',
   './js/index.js',
-  './js/idb.js'
+  './js/idb.js',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png',
+  './api/transaction'
 ];
 const APP_PREFIX = 'budget-';
 const VERSION = 'version_01';
@@ -31,6 +40,22 @@ self.addEventListener('activate', function(event) {
           return caches.delete(keyList[i]);
         }
       }));
+    })
+  )
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(`fetch request : ${event.request.url}`)
+  event.respondWith(
+    caches.match(event.request).then(function(request) {
+      if (request) {
+        console.log(`responding with cache : ${event.request.url}`)
+        return request
+      }
+      else {
+        console.log(`file is not cached, fetching : ${event.request.url}`)
+        return fetch(event.request)
+      }
     })
   )
 })
